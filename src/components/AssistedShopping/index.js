e0867326694cabebd17b70a2cfce10ec7870d29a
GusBaamonde <gusvodesign@gmail.com>
2023-12-05 11:14:08 -0300

    fix: add new kpi

--- src/components/AssistedShopping/index.js
+++ src/components/AssistedShopping/index.js
@@ -1,7 +1,6 @@
 import { Helmet } from "react-helmet";
 import { HeaderSection } from "../shared/HeaderSection/HeaderSection";
 import { Dropdown } from "../shared/Dropdown/Dropdown";
-import { Kpi } from "../shared/kpi/Kpi";
 import { AreaChart } from "./AreaChart/AreaChart";
 
 export const AssistedShoppingSection = () => {
@@ -29,83 +28,55 @@ export const AssistedShoppingSection = () => {
           </div>
           <div className="col-sm-12 col-md-4 col-lg-4 m-b-12"></div>
         </form>
-<div className="dp-wrapper-as-kpi">
-<ul>
-  <li>
-    <div className="dp-kpi-card">
-      <span className="dp-assisted-sales-icon dpicon iconapp-shopping-complete"></span>
-      <div className="dp-assisted-sales-text">
-        <h3>950</h3>
-        <span>Total de Ventas</span>
-      </div>
-    </div>
-  </li>
-  <li>
-  <div className="dp-kpi-card">
-      <span className="dp-assisted-sales-icon dpicon iconapp-dollar-money"></span>
-      <div className="dp-assisted-sales-text">
-        <h3>$18.200.00 </h3>
-        <span>Total de ganacia</span>
-      </div>
-    </div>
-  </li>
-  <li>
-  <div className="dp-kpi-card">
-      <span className="dp-assisted-sales-icon dpicon iconapp-shopping-bag"></span>
-      <div className="dp-assisted-sales-text">
-        <h3>$40.00</h3>
-        <span>Ganancia promedio</span>
-      </div>
-    </div>
-  </li>
-  <li>
-  <div className="dp-kpi-card">
-      <span className="dp-assisted-sales-icon dpicon iconapp-capital"></span>
-      <div className="dp-assisted-sales-text">
-        <h3>2.0%</h3>
-        <span>Tasa de conversión</span>
-      </div>
-    </div>
-  </li>
-  <li>
-  <div className="dp-kpi-card">
-      <span className="dp-assisted-sales-icon dpicon iconapp-sales-growth"></span>
-      <div className="dp-assisted-sales-text">
-        <h3>32 %</h3>
-        <span>Retorno Inversión</span>
-      </div>
-    </div>
-  </li>
-</ul>
-</div>
-
-        {/* <div className="dp-rowflex dp-dashboard-panel">
-          <Kpi
-            iconClass="dpicon iconapp-shopping-complete"
-            kpiValue="950"
-            kpiTitleId="Total de ventas"
-          />
-          <Kpi
-            iconClass="dpicon iconapp-dollar-money"
-            kpiValue="$18.200.00"
-            kpiTitleId="Total de ganancia"
-          />
-          <Kpi
-            iconClass="dpicon iconapp-shopping-bag"
-            kpiValue="$40.00"
-            kpiTitleId="Ganancia promedio"
-          />
-          <Kpi
-            iconClass="dpicon iconapp-shopping-bag"
-            kpiValue="2.0%"
-            kpiTitleId="Tasa de conversion"
-          />
-          <Kpi
-            iconClass="dpicon iconapp-sales-growth"
-            kpiValue="32%"
-            kpiTitleId="Retorno inversion"
-          />
-        </div> */}
+        <div className="dp-wrapper-as-kpi">
+          <ul>
+            <li>
+              <div className="dp-kpi-card">
+                <span className="dp-assisted-sales-icon dpicon iconapp-shopping-complete"></span>
+                <div className="dp-assisted-sales-text">
+                  <h3>950</h3>
+                  <span>Total de Ventas</span>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="dp-kpi-card">
+                <span className="dp-assisted-sales-icon dpicon iconapp-dollar-money"></span>
+                <div className="dp-assisted-sales-text">
+                  <h3>$18.200.00 </h3>
+                  <span>Total de ganacia</span>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="dp-kpi-card">
+                <span className="dp-assisted-sales-icon dpicon iconapp-shopping-bag"></span>
+                <div className="dp-assisted-sales-text">
+                  <h3>$40.00</h3>
+                  <span>Ganancia promedio</span>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="dp-kpi-card">
+                <span className="dp-assisted-sales-icon dpicon iconapp-capital"></span>
+                <div className="dp-assisted-sales-text">
+                  <h3>2.0%</h3>
+                  <span>Tasa de conversión</span>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="dp-kpi-card">
+                <span className="dp-assisted-sales-icon dpicon iconapp-sales-growth"></span>
+                <div className="dp-assisted-sales-text">
+                  <h3>32 %</h3>
+                  <span>Retorno Inversión</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
         <AreaChart />
       </div>
     </>
--- src/components/shared/Dropdown/Dropdown.js
+++ src/components/shared/Dropdown/Dropdown.js
@@ -1,19 +1,30 @@
 export const Dropdown = () => {
   return (
     <fieldset className="filter">
-      <label for="list-selection" class="labelcontrol" aria-disabled="false" data-required="false">Select list
-            <div class="dp-select">
-              <span class="dropdown-arrow"></span>
-              <select id="list-selection" class="" name="list-selection" aria-invalid="false">
-                <option value="">
-                  Lorem Ipsum has been the industry's standard dummy
-                </option>
-                <option value="">item list</option>
-                <option value="">item list</option>
-                <option value="">item list</option>
-              </select>
-            </div>
-            </label>
+      <label
+        for="list-selection"
+        className="labelcontrol"
+        aria-disabled="false"
+        data-required="false"
+      >
+        Select list
+        <div className="dp-select">
+          <span className="dropdown-arrow"></span>
+          <select
+            id="list-selection"
+            className=""
+            name="list-selection"
+            aria-invalid="false"
+          >
+            <option value="">
+              Lorem Ipsum has been the industry's standard dummy
+            </option>
+            <option value="">item list</option>
+            <option value="">item list</option>
+            <option value="">item list</option>
+          </select>
+        </div>
+      </label>
     </fieldset>
   );
 };
