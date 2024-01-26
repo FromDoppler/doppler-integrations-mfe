29f327d4dfc6f9e83ea0e008bef702e598584010
fgonzalez <fgonzalez@makingsense.com>
2024-01-26 18:01:02 -0300

    chore: add utility functions

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,23 @@
 export const timeout = (ms: number) =>
   new Promise((resolve) => setTimeout(resolve, ms));
+
+export const addDays = (date: Date, days: number) => {
+  const newDate = new Date(date);
+  newDate.setDate(date.getDate() + days);
+  return newDate;
+};
+
+export const getFirstDayOfLastMonth = (date: Date) => {
+  date.setDate(0);
+  date.setDate(1);
+  date.setHours(0, 0, 0, 0);
+
+  return date;
+};
+
+export const getFirstDayMonth = (date: Date) => {
+  date.setDate(1);
+  date.setHours(0, 0, 0, 0);
+
+  return date;
+};
