72c73787f42f6ac9b8a809e076717fe961728c41
fgonzalez <fgonzalez@makingsense.com>
2024-02-27 14:36:47 -0300

    chore: replace list for grid

--- src/components/shared/kpi/Kpi.js
+++ src/components/shared/kpi/Kpi.js
@@ -4,30 +4,28 @@ export const Kpi = ({ data }) => {
   const intl = useIntl();
 
   return (
-    <div className="dp-wrapper-as-kpi">
-      <ul>
-        {data.map((kpi, index) => (
-          <li key={`kpi-${index}`}>
-            <div className="dp-kpi-card">
-              <span
-                className={`dp-assisted-sales-icon dpicon iconapp-${intl.formatMessage(
-                  {
-                    id: `AssistedShopping.kpi.iconClass.${kpi.title}`,
-                  },
-                )}`}
-              ></span>
-              <div className="dp-assisted-sales-text">
-                <h3>{kpi.value}</h3>
-                <span>
-                  {intl.formatMessage({
-                    id: `AssistedShopping.kpi.${kpi.title}`,
-                  })}
-                </span>
-              </div>
+    <div className="dp-rowflex">
+      {data.map((kpi, index) => (
+        <div class="col-lg-2 m-b-12 m-r-12" key={`kpi-${index}`}>
+          <div className="dp-kpi-card">
+            <span
+              className={`dp-assisted-sales-icon dpicon iconapp-${intl.formatMessage(
+                {
+                  id: `AssistedShopping.kpi.iconClass.${kpi.title}`,
+                },
+              )}`}
+            ></span>
+            <div className="dp-assisted-sales-text">
+              <h3>{kpi.value}</h3>
+              <span>
+                {intl.formatMessage({
+                  id: `AssistedShopping.kpi.${kpi.title}`,
+                })}
+              </span>
             </div>
-          </li>
-        ))}
-      </ul>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
