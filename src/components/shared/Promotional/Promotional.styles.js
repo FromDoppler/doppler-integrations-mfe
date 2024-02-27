ca68a8104ac931b64767f9a28cb9e215b3da87ee
fgonzalez <fgonzalez@makingsense.com>
2024-02-27 16:14:35 -0300

    chore: add integrations section

--- src/components/shared/Promotional/Promotional.styles.js
+++ src/components/shared/Promotional/Promotional.styles.js
@@ -2,5 +2,11 @@ import styled from "styled-components";
 
 const PromotionalImg = (props) => <img {...props} alt={props.alt} />;
 
+const Image = styled.img`
+  max-height: 40px;
+  max-width: 100%;
+`;
+
 export const StyledPromotionalPreviewImg = styled(PromotionalImg)``;
 export const StyledPromotionalLogo = styled(PromotionalImg)``;
+export { Image };
