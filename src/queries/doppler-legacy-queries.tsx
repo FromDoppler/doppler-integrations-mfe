a9b8155dc63ac99cd11775e979de44cdb7c94883
emoralesms <emorales@makingsense.com>
2025-08-29 16:45:27 -0300

    chore: use UpdateRfmResponse

--- src/queries/doppler-legacy-queries.tsx
+++ src/queries/doppler-legacy-queries.tsx
@@ -1,24 +1,17 @@
 import { useMutation } from "@tanstack/react-query";
 import { useAppServices } from "../components/application";
-import { RfmStatus } from "../abstractions/rfm/rfm-types";
+import { RfmStatus, UpdateRfmResponse } from "../abstractions/rfm/rfm-types";
 
 export const useUpdateRfmSettings = () => {
   const { dopplerLegacyClient } = useAppServices();
 
-  return useMutation({
-    mutationFn: async ({
-      idThirdPartyApp,
-      rfm,
-    }: {
-      idThirdPartyApp: number;
-      rfm: RfmStatus;
-    }) => {
-      const result = await dopplerLegacyClient.updateRfmSettings(
-        idThirdPartyApp,
-        rfm,
-      );
-
-      return result;
+  return useMutation<
+    UpdateRfmResponse,
+    Error,
+    { idThirdPartyApp: number; rfm: RfmStatus }
+  >({
+    mutationFn: async ({ idThirdPartyApp, rfm }) => {
+      return await dopplerLegacyClient.updateRfmSettings(idThirdPartyApp, rfm);
     },
   });
 };
