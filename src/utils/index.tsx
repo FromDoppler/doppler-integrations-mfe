29c9f9a51e7c1e78b78e4af88063e4acbcaa9b7f
emoralesms <emorales@makingsense.com>
2025-08-29 11:22:29 -0300

    feat: capitalize

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -69,3 +69,6 @@ export const sanitizeDateStringToIsoFormat = (dateString: string) => {
 
   return dateString;
 };
+
+export const capitalize = (text: string): string =>
+  text ? text.charAt(0).toUpperCase() + text.slice(1) : "";
